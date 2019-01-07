9a518a81eacf0a214856f63b041dd324533b8f94
Rafael Paz Silva <rafaelpaz@petrobras.com.br>
2019-01-07 15:23:06 -0200

    first test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,10 @@
+from django.urls import resolve
 from django.test import TestCase
+from posts.views import index
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_resolve_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
