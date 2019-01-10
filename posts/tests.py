d5c721271c8aee3b2cdf31f61b89302c7b2ce9f8
Rafael Paz Silva <rafaelpaz@petrobras.com.br>
2019-01-10 10:19:23 -0200

    testing html

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,6 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from posts.views import index
 
 
@@ -8,3 +9,11 @@ class HomePageTest(TestCase):
     def test_resolve_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
